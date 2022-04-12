a3e0d59fddc38f70c455386069781d7cef1f19cd
Ashton Sobeck <asobeck@clemson.edu>
2022-04-11 23:57:53 -0400

    merge in

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
-ssl._create_default_https_context = ssl._create_unverified_context
+# ssl._create_default_https_context = ssl._create_unverified_context
 
 def make_control_model():
     # can take any height or width
