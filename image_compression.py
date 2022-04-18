5fd3fd622fd5cbfde9dd7460c85bc6d9590f5e59
Will Sherrer <wsherre@clemson.edu>
2022-04-18 00:00:35 -0400

    Added some graph support

--- image_compression.py
+++ image_compression.py
@@ -2,8 +2,10 @@ import numpy as np
 from numpy.linalg import svd
 import sys
 import time
+import matplotlib as plt
 
-#compression amount should be 0 and 1. Where 20% compression is 0.2
+#compression amount should be 0 and 1. Where 20% compression is 0.2 or compression_amount can equal amount of columns you want compressed. 
+#therefor input a number between 1 and max_columns(32 for cifar) k = amount of columns of U used. 
 def compress_images(images, compression_amount=0.2):
     
     #images should be of size (batch_size, 32, 32, 3)
@@ -11,7 +13,10 @@ def compress_images(images, compression_amount=0.2):
 
     total = images.shape[0]
     #this is the amount of columns of U that will be used in the images compression
-    k = int((1 - compression_amount) * 32)
+    if compression_amount >= 1:
+        k = compression_amount
+    else:
+        k = int((1 - compression_amount) * 32)
     begin_time = time.time()
     eta = 0
 
@@ -55,5 +60,67 @@ def compress_images(images, compression_amount=0.2):
 
     return compressed_images
 
+
+
+def plot_graphs(training_data_control, training_data_other, compress_list):
+    # plot accuracy over training time
+    train_legend = []
+    for i, train in enumerate(training_data_control):
+        plt.plot(train.history['accuracy'])
+        plt.plot(train.history['val_accuracy'])
+        train_legend.append("Train Acc %s Columns" %(compress_list[i]))
+        train_legend.append("Test Acc %s Columns" %(compress_list[i]))
+
+    plt.title('Accuracy Over Time Control Model')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend(train_legend)
+    plt.savefig('figures/acc_over_time_control.png')
+    plt.show()
+
+    train_legend = []
+    for i, train in enumerate(training_data_other):
+        plt.plot(train.history['accuracy'])
+        plt.plot(train.history['val_accuracy'])
+        train_legend.append("Train Acc %s Columns" %(compress_list[i]))
+        train_legend.append("Test Acc %s Columns" %(compress_list[i]))
+
+    plt.title('Accuracy Over Time 2nd Model')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend(train_legend)
+    plt.savefig('figures/acc_over_time_2nd_model.png')
+    plt.show()
+
+
+    # plot loss over training time
+    train_legend = []
+    for i, train in enumerate(training_data_control):
+        plt.plot(train.history['loss'])
+        plt.plot(train.history['val_loss'])
+        train_legend.append("Train Loss %s Columns" %(compress_list[i]))
+        train_legend.append("Test Loss %s Columns" %(compress_list[i]))
+
+    plt.title('Loss Over Time Control Model')
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.legend(train_legend)
+    plt.savefig('figures/loss_over_time_control.png')
+    plt.show()
+
+    train_legend = []
+    for i, train in enumerate(training_data_other):
+        plt.plot(train.history['loss'])
+        plt.plot(train.history['val_loss'])
+        train_legend.append("Train Loss %s Columns" %(compress_list[i]))
+        train_legend.append("Test Loss %s Columns" %(compress_list[i]))
+
+    plt.title('Loss Over Time 2nd Model')
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.legend(train_legend)
+    plt.savefig('figures/loss_over_time_2nd_model.png')
+    plt.show()
+    
 
 
--- model.py
+++ model.py
@@ -10,9 +10,10 @@ import ssl
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from image_compression import compress_images
+from image_compression import compress_images, plot_graphs
 import numpy as np
 from model_2 import make_mode_2
+import os
 # ssl._create_default_https_context = ssl._create_unverified_context
 
 def make_control_model():
@@ -39,78 +40,86 @@ def main():
     learning_rate = 1e-3
     batch_size = 32
     epochs = 100
+    img_size = 32
     
     (train_imgs, train_labels), (test_imgs, test_labels) = keras.datasets.cifar10.load_data()
 
-    #only do first image if you want to just test the compression and compare the images
-    '''x = compress_images(train_imgs[0].reshape(1, 32, 32, 3), 0.90)
-    plt.imshow(x[0])
-    plt.imshow(train_imgs[0])'''
+    #list of columns to be used. 32 for all columns (0% compression), 1 for least columns
+    compress_list = [32, 6, 4, 2, 1]
 
-    #Takes about 12-15 seconds on my mac to compress just the training images
-    compressed_images = compress_images(train_imgs, 0.90)
-
-    control_model = make_control_model()
-    #control_model.summary()
-    other_model = make_mode_2()
-    #other_model.summary()
-
-    other_model.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate), 
-                          loss=keras.losses.SparseCategoricalCrossentropy(),
-                          metrics=[keras.metrics.SparseCategoricalAccuracy()]
-                          )
-
-    
-    control_model.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate), 
-                          loss=keras.losses.SparseCategoricalCrossentropy(),
-                          metrics=[keras.metrics.SparseCategoricalAccuracy()]
-                          )
-    train = control_model.fit(train_imgs, 
-                      train_labels, 
-                      epochs=epochs,
-                      batch_size=batch_size,
-                      callbacks=[
-                          keras.callbacks.ModelCheckpoint('./model_save.ckpt', 
-                                                          monitor="val_loss",
-                                                          mode="max",
-                                                          save_freq='epoch',
-                                                          save_best_only=True)  
-                      ],
-                      validation_data=(test_imgs,test_labels)
-                      )
-
-    train_2 = other_model.fit(train_imgs, 
-                      train_labels, 
-                      epochs=epochs,
-                      batch_size=batch_size,
-                      callbacks=[
-                          keras.callbacks.ModelCheckpoint('./model_save.ckpt', 
-                                                          monitor="val_loss",
-                                                          mode="max",
-                                                          save_freq='epoch',
-                                                          save_best_only=True)  
-                      ],
-                      validation_data=(test_imgs,test_labels)
-                      )
-
-    # plot accuracy and lost over training time
-    plt.plot(train.history['accuracy'])
-    plt.plot(train.history['val_accuracy'])
-    plt.title('Accuracy Over Time')
-    plt.ylabel('accuracy')
-    plt.xlabel('epochs')
-    plt.legend(['train accuracy','test accuracy'])
-    plt.savefig('./acc_over_time.png')
+    #this block of code is just to show a test img real quick if you want to with all the compressed images
+    rows, col = 2, 3
+    for i in range(len(compress_list)):
+        x = compress_images(train_imgs[4].reshape(1, img_size, img_size, 3), compress_list[i])
+        plt.subplot(rows, col, i + 1)
+        #can print columns or percentage in same compress list
+        if compress_list[i] >= 1:
+            plt.title("%s columns" % (compress_list[i]))
+        else:
+            plt.title("%s%%" % (compress_list[i]))
+        plt.imshow(x[0])
+    #since it prints after each compress images, just clean up the terminal
+    os.system("clear")
     plt.show()
-    
-    plt.plot(train.history['loss'])
-    plt.plot(train.history['val_loss'])
-    plt.title('Loss Over Time')
-    plt.ylabel('loss')
-    plt.xlabel('epochs')
-    plt.legend(['train loss','test loss'])
-    plt.savefig('./loss_over_time.png')
-    plt.show()
+
+    training_data_control = []
+    training_data_other = []
+    for i in range(len(compress_list)):
+        columns = compress_list[i]
+        #Takes about 12-15 seconds on my mac to compress just the training images
+        if columns < img_size:
+            train_imgs = compress_images(train_imgs, columns)
+            test_imgs = compress_images(test_imgs, columns)
+
+        control_model = make_control_model()
+        #control_model.summary()
+        other_model = make_mode_2()
+        #other_model.summary()
+
+        other_model.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate), 
+                            loss=keras.losses.SparseCategoricalCrossentropy(),
+                            metrics=[keras.metrics.SparseCategoricalAccuracy()]
+                            )
+
+        
+        control_model.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate), 
+                            loss=keras.losses.SparseCategoricalCrossentropy(),
+                            metrics=[keras.metrics.SparseCategoricalAccuracy()]
+                            )
+        train = control_model.fit(train_imgs, 
+                        train_labels, 
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        callbacks=[
+                            keras.callbacks.ModelCheckpoint('./model_save.ckpt', 
+                                                            monitor="val_loss",
+                                                            mode="max",
+                                                            save_freq='epoch',
+                                                            save_best_only=True)  
+                        ],
+                        validation_data=(test_imgs,test_labels)
+                        )
+
+        train_2 = other_model.fit(train_imgs, 
+                        train_labels, 
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        callbacks=[
+                            keras.callbacks.ModelCheckpoint('./model_save.ckpt', 
+                                                            monitor="val_loss",
+                                                            mode="max",
+                                                            save_freq='epoch',
+                                                            save_best_only=True)  
+                        ],
+                        validation_data=(test_imgs,test_labels)
+                        )
+        training_data_control.append(train)
+        training_data_other.append(train_2)
+
+    #create folder for figures
+    os.makedirs("figures", exist_ok=True)
+    #typed it all out to ensure I didn't mess it up. Plotting graphs hasn't been fully tested yet haha
+    plot_graphs(training_data_control=training_data_control, training_data_other=training_data_other, compress_list=compress_list)
 
 if __name__ == "__main__":
     main()
